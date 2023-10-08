bb3ac0e0ae23ceb88fa4009ee43c84793656d641
zozonteq <ottotak@outlook.com>
2023-10-08 20:59:21 +0900

    URLを読み上げないように変更

--- main.py
+++ main.py
@@ -6,66 +6,69 @@ import json
 import wave
 from gradio_client import Client
 import os
-
+import uuid
+from src import checker
 
 #ayaka-jp_e101.pth
 #hutao-jp.pth
 
-rvc_client = Client("http://localhost:7865/")
-result = rvc_client.predict(
-				"yanfei-jp.pth",	# 推論ファイル
-				0,
-				0,
-				api_name="/infer_change_voice"
-)
-print(result)
-
-
-def rvc(filepath):
-    result = rvc_client.predict(
-				0,	
-				filepath,	
-				0,	
-				filepath,
-				"pm",	
-				"Howdy!",	
-				"null",	
-				0,	
-				0,	
-				0,	
-				0,	
-				0,	
-				api_name="/infer_convert"
-    )
-    return result[1]
-
-def generate_wav(text, speaker=1, filepath='./temp/wav/audio.wav'):
-    host = 'localhost'
-    port = 50021
-    params = (
-        ('text', text),
-        ('speaker', speaker),
-        ("enable_interrogative_upspeak",True)
-    )
-    response1 = requests.post(
-        f'http://{host}:{port}/audio_query',
-        params=params
-    )
-    print(response1.json())
-    headers = {'Content-Type': 'application/json',}
-    response2 = requests.post(
-        f'http://{host}:{port}/synthesis',
-        headers=headers,
-        params=params,
-        data=json.dumps(response1.json())
-    )
-
-    with open(filepath,mode="wb") as f:
-        f.write(response2.content)
-        f.close()
-    
 
 if __name__ == "__main__":
+    rvc_pitch = 0 
+    rvc_client = Client("http://localhost:7865/")
+    result = rvc_client.predict(
+                    "hutao-jp.pth",	# 推論ファイル
+                    0,
+                    0,
+                    api_name="/infer_change_voice"
+    )
+
+
+    def rvc(filepath):
+        result = rvc_client.predict(
+                    0,	
+                    filepath,	
+                    rvc_pitch,	#ピッチ
+                    filepath,
+                    "pm",	# pm or harvest or crepe
+                    "Howdy!",	
+                    "null",	
+                    0,	
+                    0,	
+                    0,	
+                    0,	
+                    0,	
+                    api_name="/infer_convert"
+        )
+        print("rvc_pitch:"+str(rvc_pitch));
+        return result[1]
+
+    def generate_wav(text, speaker=1, filepath='./temp/wav/audio.wav'):
+        host = 'localhost'
+        port = 50021
+        params = (
+            ('text', text),
+            ('speaker', speaker),
+            ("enable_interrogative_upspeak",True)
+        )
+        response1 = requests.post(
+            f'http://{host}:{port}/audio_query',
+            params=params
+        )
+        print(response1.json())
+        headers = {'Content-Type': 'application/json',}
+        response2 = requests.post(
+            f'http://{host}:{port}/synthesis',
+            headers=headers,
+            params=params,
+            data=json.dumps(response1.json())
+        )
+
+        with open(filepath,mode="wb") as f:
+            f.write(response2.content)
+            f.close()
+        
+
     discord_access_token = "";
     discord_application_id = "";
 
@@ -100,17 +103,22 @@ if __name__ == "__main__":
     async def join_command(interaction:discord.Interaction):
         await interaction.guild.voice_client.disconnect()
         await interaction.response.send_message("切断しました。")
+    @tree.command(name="vpitch",description="ピッチを調節します。")
+    async def pitch_command(interaction:discord.Interaction,value:int):
+        global rvc_pitch 
+        rvc_pitch = value
+        await interaction.response.send_message(f"ピッチを{value}に設定しました。")
     @client.event
     async def on_message(message):
-        if message.author.bot:
-            return
-        else:
+        if not message.author.bot:
             if message.guild.voice_client is not None:
-                generate_wav(message.content,2,f"temp/wav/{message.content}.wav")
-                print("input:"+str(os.path.abspath(f"temp/wav/{message.content}.wav")))
-                rvc_voice_path = rvc(os.path.abspath(f"temp/wav/{message.content}.wav"))
-                print("Playing:"+rvc_voice_path)
+                content = message.content
+                if checker.is_url(content):
+                    content = "URL"
+                
+                msg_uuid = str(uuid.uuid4())
+                generate_wav(content,2,f"temp/wav/{msg_uuid}.wav")
+                rvc_voice_path = rvc(os.path.abspath(f"temp/wav/{msg_uuid}.wav"))
                 message.guild.voice_client.play(discord.FFmpegPCMAudio(rvc_voice_path))
-                print(message.content)
 
     client.run(discord_access_token)
