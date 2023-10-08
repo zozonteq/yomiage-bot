7fb688dd0399136d490fceac6ee0a342bf7164a7
zozonteq <ottotak@outlook.com>
2023-10-08 21:48:50 +0900

    ジョッキミュージックなどのコマンドを無視するように

--- src/checker.py
+++ src/checker.py
@@ -2,3 +2,9 @@ import re
 
 def is_url(text):
     return bool(re.match("https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+",text))
+def is_bot_command(text):# Jockie MusicなどのBotコマンドかどうかをチェック
+    bot_commands = ("m!","M!",";;")
+    return text.startswith(bot_commands)
+
+def ignore_check(text):
+    return is_bot_command(text) or is_url(text)
